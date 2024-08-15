9537ece94c514ce5d6f41759b27f8cb0fc783f31
Andre Silva <andreswebs@pm.me>
2024-08-15 08:07:33 -0300

    upd auth

--- .github/actions/gh-app-auth/scripts/gh-app-token.sh
+++ .github/actions/gh-app-auth/scripts/gh-app-token.sh
@@ -14,9 +14,8 @@ set -o nounset
 ## And GitHub:
 ## https://docs.github.com/en/apps/creating-github-apps/authenticating-with-a-github-app/generating-a-json-web-token-jwt-for-a-github-app
 
-GITHUB_APP_ID="${GITHUB_APP_ID:-}"
-GITHUB_APP_CLIENT_ID="${GITHUB_APP_CLIENT_ID:-}"
-GITHUB_APP_PRV_KEY_PATH="${GITHUB_APP_PRV_KEY_PATH:-}"
+GH_APP_OR_CLIENT_ID="${GH_APP_OR_CLIENT_ID:-}"
+GH_APP_PRV_KEY_PATH="${GH_APP_PRV_KEY_PATH:-}"
 
 EXPIRATION_SECONDS="${EXPIRATION_SECONDS:-600}" # default 10 minutes
 ISSUE_START_SECONDS="${ISSUE_START_SECONDS:-60}" # default 60 seconds in the past
@@ -28,25 +27,18 @@ err_log() {
 
 check_cmd() {
   if ! command -v "${1}" &> /dev/null; then
-    err_log "error: ${1} must be present to run this script"
+    err_log "error: ${1} command is missing; you must check how to install it"
     exit 1
   fi
 }
 
-iss="${GITHUB_APP_CLIENT_ID}"
-
-[ -z "${iss}" ] && {
-  # fallback to GITHUB_APP_ID
-  iss="${GITHUB_APP_ID}"
-}
-
-[ -z "${iss}" ] && {
-  err_log "error: GITHUB_APP_ID or GITHUB_APP_CLIENT_ID must be set"
+[ -z "${GH_APP_OR_CLIENT_ID}" ] && {
+  err_log "error: GH_APP_OR_CLIENT_ID must be set"
   exit 1
 }
 
-[ ! -f "${GITHUB_APP_PRV_KEY_PATH}" ] && {
-  err_log "error: GITHUB_APP_PRV_KEY_PATH must refer to a file"
+[ ! -f "${GH_APP_PRV_KEY_PATH}" ] && {
+  err_log "error: GH_APP_PRV_KEY_PATH must refer to a file"
   exit 1
 }
 
@@ -54,7 +46,8 @@ check_cmd openssl
 check_cmd curl
 check_cmd jq
 
-secret=$(cat "${GITHUB_APP_PRV_KEY_PATH}")
+iss="${GH_APP_OR_CLIENT_ID}"
+secret=$(cat "${GH_APP_PRV_KEY_PATH}")
 
 now=$(date +%s)
 iat=$((${now} - ${ISSUE_START_SECONDS})) # Issues x seconds in the past
@@ -88,8 +81,8 @@ signature=$(
     b64enc
 )
 
-GITHUB_JWT="${content}.${signature}"
-BEARER="Authorization: Bearer ${GITHUB_JWT}"
+GH_JWT="${content}.${signature}"
+BEARER="Authorization: Bearer ${GH_JWT}"
 ACCEPT="Accept: application/vnd.github.v3+json"
 INSTALLATIONS_API_URL="https://api.github.com/app/installations"
 
@@ -109,7 +102,7 @@ APP_TOKEN_URL=$(
     jq --raw-output '.[0].access_tokens_url' # single installation
 )
 
-GITHUB_TOKEN=$(
+GH_TOKEN=$(
     curl \
         --request POST \
         --silent \
@@ -119,4 +112,4 @@ GITHUB_TOKEN=$(
     jq --raw-output '.token'
 )
 
-echo "${GITHUB_TOKEN}"
+echo "${GH_TOKEN}"
